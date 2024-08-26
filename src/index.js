f82ec69415d42b8510e2086fcee8d6ac702fa658
tearzday <81463394+tearzday@users.noreply.github.com>
2024-08-26 17:00:45 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    if (n <= 0) {
+        return n
+    }
+    else {
+        return n % 10 * (10**(n.toString().length - 1)) + reverse(Math.trunc(n / 10))
+    }
 }
